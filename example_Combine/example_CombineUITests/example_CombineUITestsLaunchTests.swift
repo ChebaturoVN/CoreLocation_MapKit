03278155190036fd1eee6a498d1943c8655d9adb
ChebaturoVN <kukuruza2488@gmail.com>
2023-07-20 13:02:33 +0300

    without_Soryboard
    without_Soryboard

---
+++ example_Combine/example_CombineUITests/example_CombineUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  example_CombineUITestsLaunchTests.swift
+//  example_CombineUITests
+//
+//  Created by VladimirCH on 20.07.2023.
+//
+
+import XCTest
+
+final class example_CombineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
